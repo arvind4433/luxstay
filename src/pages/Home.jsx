a69912c29a55c0f38546a075ed9709f63e4a4ef7
Arvind Singh <arvind889481@gmail.com>
2025-12-04 14:46:49 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import Header from "../components/Header";
 import Video from "../pages/Video";
 import Footer from "../components/Footer";
-import Facilities from "./temp"
+import Facilities from "./Facilities"
 import Information from "../pages/Information"
 
 
